ae04d624aa182223596358da685c97f9f8a29390
Gourav Sharma <gourav17112002@gmail.com>
2024-08-31 02:37:54 +0530

    css styling problem resolved

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.delete('/listings/:id',async(req,res)=>{
 });
 
 // edit
-app.get('/listing/edit/:id',async(req,res)=>{
+app.get('/listings/edit/:id',async(req,res)=>{
     let {id}=req.params;
     let data=await listingModels.findById(id);
     res.render('edit.ejs');
